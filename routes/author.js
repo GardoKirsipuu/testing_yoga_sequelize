5c219cb680232824a63b5ce6f8a698787476e9bc
GardoKirsipuu <gardo1999@gmail.com>
2022-02-19 22:54:04 +0200

    Getted authors's articles data

--- routes/author.js
+++ routes/author.js
@@ -2,10 +2,10 @@ const express = require('express');
 // get using express router
 const router = express.Router();
 // define article controller and export it for this file
-const authorController = require('../controllers/article');
+const authorController = require('../controllers/author');
 
 // use controller functions according to the route
-router.get('/author/:author_id', authorController.getArticlesByAuthor);
+router.get('/:id', authorController.getArticlesByAuthor);
 
 // export article router for using in default application file
 module.exports = router;
